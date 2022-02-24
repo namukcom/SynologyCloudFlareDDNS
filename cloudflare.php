e180e1b0b99e5d62412dfb40378daba5241ab1ec
Albred <nina514@gmail.com>
2022-02-24 16:53:00 +0900

    curl 호출부 함수화 및 for문 제거

--- cloudflare.php
+++ cloudflare.php
@@ -19,11 +19,11 @@ if (strpos($hostname, '.') === false) {
 }
 if(strlen($pwd) == 37) /* Global key 37byte*/
 {
-	$header = array("X-Auth-Email: ${account}", "X-Auth-Key: ${pwd}", "Content-Type: application/json");
+    $header = array("X-Auth-Email: ${account}", "X-Auth-Key: ${pwd}", "Content-Type: application/json");
 }
 else /* API Token 40byte*/
 {
-	$header = array("Authorization: Bearer ${pwd}", "Content-Type: application/json");
+    $header = array("Authorization: Bearer ${pwd}", "Content-Type: application/json");
 }
 
 // only for IPv4 format
@@ -32,120 +32,127 @@ if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
     exit();
 }
 
-/* 
+/*
 1. Check Validity && Query Zone ID
 */
-$url = "https://api.cloudflare.com/client/v4/zones";
-
-$req = curl_init();
-
 $options = array(
-	CURLOPT_URL => $url,
-	CURLOPT_HTTPGET => true,
-	CURLOPT_HEADER => false,
-	CURLOPT_VERBOSE => false,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_HTTPHEADER => $header
+    CURLOPT_URL => "https://api.cloudflare.com/client/v4/zones",
+    CURLOPT_HTTPGET => true,
+    CURLOPT_HEADER => false,
+    CURLOPT_VERBOSE => false,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => $header
 );
 
-curl_setopt_array($req, $options);
-$res = curl_exec($req);
-curl_close($req);
-$json = json_decode($res, true);
-
-if ('false' != $json['success']) {
-	echo 'badauth';
-	exit();
+if (empty($data = exec_curl($options))) {
+    echo 'badauth';
+    exit();
 }
 
-$domain_total = $json['result_info']['total_count'];
+$zone_id = -1;
+$result = array_filter(array_get($data, 'result', []), function($row) use ($hostname) {
+    return preg_match('/'.$row['name'].'$/i', $hostname) > 0;
+});
 
-$zoneID = -1;
-for ($i = 0; $i < $domain_total; $i++) {
-	$domain = (string)$json['result'][$i]['name'];
-	if (substr( $hostname, strlen( $hostname ) - strlen( $domain ) ) === $domain){
-		$zoneID = $json['result'][$i]['id'];
-		break;
-	}
-}
-
-if ($zoneID === -1) {
+if (empty($zone_info = array_pop($result))) {
     echo 'nohost';
     exit();
 }
+$zone_id = $zone_info['id'];
 
 /*
 2. Query Record ID
 */
-$url = "https://api.cloudflare.com/client/v4/zones/${zoneID}/dns_records?type=A&name=${fullname}";
-
-$req = curl_init();
-
 $options = array(
-	CURLOPT_HTTPGET=>true,
-	CURLOPT_URL=>$url,
-	CURLOPT_HEADER=>false,
-	CURLOPT_VERBOSE=>false,
-	CURLOPT_RETURNTRANSFER=>true,
-	CURLOPT_HTTPHEADER=> $header
+    CURLOPT_URL => "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?type=A&name=${fullname}",
+    CURLOPT_HTTPGET => true,
+    CURLOPT_HEADER => false,
+    CURLOPT_VERBOSE => false,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER =>  $header
 );
 
-curl_setopt_array($req, $options);
-$res = curl_exec($req);
-curl_close($req);
-$json = json_decode($res, true);
-
-if ('false' != $json['success']) {
-	echo 'badauth';
-	exit();
+if (empty($data = exec_curl($options))) {
+    echo 'badauth';
+    exit();
 }
 
-if(1 != $json['result_info']['total_count'])
-{
-	echo 'nohost';
-	exit();
+$result = array_filter(array_get($data, 'result', []), function($row) use ($hostname) {
+    return $row['name'] === $hostname;
+});
+
+if(empty($record_info = array_pop($result))) {
+    echo 'nohost';
+    exit();
 }
 
-$recordID = $json['result'][0]['id'];
-$ttl = $json['result'][0]['ttl'];
-$proxied = $json['result'][0]['proxied'];
+$record_id = $record_info['id'];
+$ttl = $record_info['ttl'];
+$proxied = $record_info['proxied'];
 
 /*
 3. Update DNS
 */
-$url = "https://api.cloudflare.com/client/v4/zones/${zoneID}/dns_records/$recordID";
-$post = array(
-    'type'=>'A',
-    'name'=>$fullname,
-    'content'=>$ip,
-    'ttl'=>$ttl,
-    'proxied'=>$proxied
-	
-);
-
-$req = curl_init();
-
 $options = array(
-	CURLOPT_URL => $url,
-	CURLOPT_HTTPGET => false,
-	CURLOPT_CUSTOMREQUEST => "PUT",	
-	CURLOPT_HEADER => false,
-	CURLOPT_VERBOSE => false,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_HTTPHEADER => $header,
-	CURLOPT_POST => false,
-	CURLOPT_POSTFIELDS => json_encode($post)
+    CURLOPT_URL => "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${record_id}",
+    CURLOPT_POST => true,
+    CURLOPT_CUSTOMREQUEST => "PUT",
+    CURLOPT_HEADER => false,
+    CURLOPT_VERBOSE => false,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => $header,
+    CURLOPT_POSTFIELDS => json_encode(array(
+        'type'=>'A',
+        'name'=>$fullname,
+        'content'=>$ip,
+        'ttl'=>$ttl,
+        'proxied'=>$proxied
+    ))
 );
 
-curl_setopt_array($req, $options);
-$res = curl_exec($req);
-curl_close($req);
-$json = json_decode($res, true);
-
-if ('false' != $json['success']) {
-	echo 'Update Record failed';
-	exit();
+if (empty($data = exec_curl($options))) {
+    echo 'Update Record failed';
+    exit();
 }
 
-printf("good");
-?>
+echo 'good';
+
+
+
+/*
+ * Helpers
+ */
+
+/**
+ * Get array member by key
+ * @param $array
+ * @param $key
+ * @param $default
+ * @return mixed|null
+ */
+function array_get(&$array, $key, $default = null) {
+    return isset($array[$key]) ? $array[$key] : $default;
+}
+
+/**
+ * execute curl and parse return data
+ * @param $options
+ * @return mixed|null
+ */
+function exec_curl($options) {
+    $req = curl_init();
+    curl_setopt_array($req, $options);
+    $res = curl_exec($req);
+    curl_close($req);
+    $result = json_decode($res, true);
+
+    // echo "\n\n";
+    // echo var_export($result, true);
+    // echo "\n\n";
+
+    if (array_get($result, 'success', false)) {
+        return $result;
+    }
+
+    return null;
+}
